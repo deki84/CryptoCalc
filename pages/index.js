9635e8df1710dddc2cae599dad26a6596ee12d36
Dejan Jankovic <deki1984@hotmail.de>
2022-09-21 23:02:16 +0200

    create Inputcomponent

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 import Logo from '../public/Logo.png';
+import Input from './Input';
 
 export default function Home() {
   return (
@@ -11,49 +12,27 @@ export default function Home() {
         </Div>
         <Headline>CryptoCalc</Headline>
         <Form>
-          <Input1
-            id="name"
-            type="text"
-            placeholder="Type here your Monthly Expenses in €"
-          />
-          <Input1
-            id="name"
-            type="text"
-            placeholder="Type here your crypto value "
-          />
-          <Button>Click me</Button>
+          <Input />
         </Form>
-        <p>Your current value is ...</p>
+        <Text>Your current value is ...</Text>
       </div>
     </>
   );
 }
+const Text = styled.p`
+  display: flex;
+  justify-content: center;
+`;
 
 const Div = styled.div`
   display: flex;
   justify-content: center;
 `;
 
-const Button = styled.button`
-  margin-left: 148px;
-  width: 87px;
-  height: 30px;
-  color: white;
-  background-color: black;
-  border: none;
-  border-radius: 10px;
-  margin-top: 10px;
-`;
-
 const Form = styled.form`
   display: flex;
   align-items: center;
   flex-direction: column;
-`;
-
-const Input1 = styled.input`
-  width: 235px;
-  height: 25px;
 `;
 
 const Headline = styled.h1`
