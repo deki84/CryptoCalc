610cf2f60d9df76f89857ca9621c9ffcbd41cf06
Dejan Jankovic <deki1984@hotmail.de>
2022-09-21 18:35:00 +0200

    create input fields and Button

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,44 @@ export default function Home() {
   return (
     <div>
       <Headline>CryptoCalc</Headline>
+      <Form>
+        <Input1
+          id="name"
+          type="text"
+          placeholder="Type here your Monthly Expenses in €"
+        />
+        <Input1
+          id="name"
+          type="text"
+          placeholder="Type here your crypto value "
+        />
+        <Button>Click me</Button>
+      </Form>
     </div>
   );
 }
+
+const Button = styled.button`
+  margin-left: 148px;
+  width: 87px;
+  height: 30px;
+  color: white;
+  background-color: black;
+  border: none;
+  border-radius: 10px;
+  margin-top: 10px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Input1 = styled.input`
+  width: 235px;
+  height: 25px;
+`;
 
 const Headline = styled.h1`
   display: flex;
