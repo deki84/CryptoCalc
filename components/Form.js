267d69c3a7f0294b14eeb98b3be84cb8a62434ac
Dejan Jankovic <deki1984@hotmail.de>
2022-09-26 15:23:49 +0200

    add function calculate

---
+++ components/Form.js
@@ -0,0 +1,80 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+
+export default function Form({ onCalculatePrice, value }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const expenses = form.expenses.value;
+    const cryptovalue = form.cryptovalue.value;
+
+    onCalculatePrice(cryptovalue, expenses);
+  }
+  const [show, setShow] = useState(false);
+  const [num, setNum] = useState('');
+  const handleNumChange = (event) => {
+    const limit = 9;
+    setNum(event.target.value.slice(0, limit));
+  };
+
+  const [num1, setNum1] = useState('');
+  const handleNumChange1 = (event) => {
+    const limit = 9;
+    setNum1(event.target.value.slice(0, limit));
+  };
+
+  return (
+    <>
+      <StyledForm
+        onSubmit={handleSubmit}
+        onKeyPress={(e) => !/[0-9]/.test(e.key) && e.preventDefault()}
+      >
+        <Inputfield
+          id="expenses"
+          name="expenses"
+          value={num}
+          onChange={handleNumChange}
+          type="number"
+          placeholder="Type here your Monthly Expenses €"
+          required
+        />
+        <Inputfield
+          id="cryptovalue"
+          name="cryptovalue"
+          value={num1}
+          onChange={handleNumChange1}
+          type="number"
+          placeholder="Type here your crypto value "
+          required
+        />
+        <Button onClick={() => setShow(!show)}>Click me</Button>
+        {show ? <Text>Your current value is {value} </Text> : null}
+      </StyledForm>
+    </>
+  );
+}
+
+const Text = styled.p`
+  display: flex;
+  justify-content: center;
+`;
+const Inputfield = styled.input`
+  width: 235px;
+  height: 25px;
+`;
+
+const Button = styled.button`
+  margin-left: 148px;
+  width: 87px;
+  height: 30px;
+  color: white;
+  background-color: black;
+  border: none;
+  border-radius: 10px;
+  margin-top: 10px;
+`;
+const StyledForm = styled.form`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 import Logo from '../public/Logo.png';
-import Input from './Input';
+import Form from '../components/Form';
+import { useState } from 'react';
 
 export default function Home() {
+  let [value, setValue] = useState();
+
+  function calculatePrice(value, expense) {
+    const bitcoin = 20000;
+    const result = (value * bitcoin) / expense;
+
+    function calculate() {
+      return Number(result).toFixed(2);
+    }
+    setValue(calculate);
+  }
+
   return (
     <>
       <div>
@@ -11,14 +24,12 @@ export default function Home() {
           <Image layout="intrinsic" src={Logo} alt="something has failed " />
         </Div>
         <Headline>CryptoCalc</Headline>
-        <Form>
-          <Input />
-        </Form>
-        <Text>Your current value is ...</Text>
+        <Form onCalculatePrice={calculatePrice} value={value} />
       </div>
     </>
   );
 }
+
 const Text = styled.p`
   display: flex;
   justify-content: center;
@@ -27,12 +38,6 @@ const Text = styled.p`
 const Div = styled.div`
   display: flex;
   justify-content: center;
-`;
-
-const Form = styled.form`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
 `;
 
 const Headline = styled.h1`
