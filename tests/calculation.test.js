ffb3e6a3ab2827072701ec05708b9def7332d3ec
Dejan Jankovic <deki1984@hotmail.de>
2022-10-12 17:34:36 +0200

    delete Timout function and allows decimal numbers

--- tests/calculation.test.js
+++ tests/calculation.test.js
@@ -2,13 +2,13 @@ import calculation from '../utils/calculation';
 
 describe('calculation', () => {
   test('calculation', () => {
-    expect(calculation(3, 2000, 20000)).toBe('30');
+    expect(calculation(3, 2000, 20000)).toBe('30.00');
   });
 
   test('calculation', () => {
-    expect(calculation(2, 2000, 20000)).toBe('20');
+    expect(calculation(2, 2000, 20000)).toBe('20.00');
   });
   test('calculation', () => {
-    expect(calculation(2, 2000, 10000)).toBe('10');
+    expect(calculation(2, 2000, 10000)).toBe('10.00');
   });
 });
--- utils/calculation.js
+++ utils/calculation.js
@@ -1,7 +1,7 @@
 export default function calculation(value, expense, price) {
   let result = 0;
   if (expense) {
-    result = ((value * price) / expense).toFixed(0);
+    result = ((value * price) / expense).toFixed(2);
   }
   return result;
 }
