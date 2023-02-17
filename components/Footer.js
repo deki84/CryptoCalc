4b67ff02326bfca43a844b396068adbbd190bce5
Dejan Jankovic <deki1984@hotmail.de>
2023-02-17 18:06:38 +0100

    change Copyright date

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <Copyright>
       <FooterText>CryptoCalc</FooterText>
-      <Rights>All Rights Reserved 2022</Rights>
+      <Rights>All Rights Reserved 2023</Rights>
+      <Founder> Deki84 </Founder>
     </Copyright>
   );
 }
@@ -22,8 +23,14 @@ const Copyright = styled.div`
 const Rights = styled.p`
   font-family: 'PT Sans', sans-serif;
   margin: 0;
-  margin-bottom: 30px;
+  margin-bottom: 10px;
 `;
 const FooterText = styled.p`
-  margin: 5px;
+  margin: 2px;
+`;
+const Founder = styled.p`
+  position: absolute;
+  color: black;
+  font-family: 'Marko One', serif;
+  margin-top: 40px;
 `;
